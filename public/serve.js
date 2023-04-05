08be0468198dacd1081c545f4232e077cb02a504
Alexsandro Gomes <alexsandro.gomes.clemente@hotmail.com>
2023-04-05 12:47:23 -0300

    Correction build

--- public/serve.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = require("./http");
-require("./websocket");
-http_1.serveHttp.listen(process.env.PORT, () => console.log('Servidor rodando na porta: ', process.env.PORT, "✔"));
